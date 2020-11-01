91e211256e9e09f1b5c9e12406cf138d90e673df
Mathtzt <matheus.cn10@gmail.com>
2020-11-01 19:34:48 -0300

    changing jdk version and refactoring all project

---
+++ pointdopao/src/java/Util/Conn.java
@@ -0,0 +1,28 @@
+package util;
+import java.sql.*;
+import java.util.Properties;
+
+public class Conn {
+  
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        if (connection != null)
+            return connection;
+        else {
+            try {
+                Properties prop = new Properties();
+                
+                String user = "admin";
+                String password = "admin";
+                //Class.forName("com.mysql.jdbc.Driver"); // Para quem for usar MySql
+                Class.forName("org.postgresql.Driver");// Para quem for usar Postgres
+                //connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/projetoIntegrador", user, password);// Para quem for usar MySql
+                connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/pointdopao",user, password);// Para quem for usar Postgres
+            } catch (ClassNotFoundException | SQLException e) {
+                System.out.println(e.getMessage());
+            }
+            return connection;
+        }
+    }
+}
