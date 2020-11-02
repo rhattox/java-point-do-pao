faed92c1b840022d4028fdd9c11a4770be690afb
bcovies <bcovies@gmail.com>
2020-11-02 17:25:56 -0300

    Adicionado Controllers do Usuário

--- pointdopao/src/java/Dao/UsuarioDao.java
+++ pointdopao/src/java/Dao/UsuarioDao.java
@@ -8,6 +8,7 @@ package Dao;
 import Models.Usuario;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import util.ConexaoBD;
 
 /**
@@ -15,20 +16,48 @@ import util.ConexaoBD;
  * @author bcovies
  */
 public class UsuarioDao {
-    
+
     private Connection connection;
-    
-    public UsuarioDao(){
+
+    public UsuarioDao() {
         connection = ConexaoBD.getConnection();
     }
-    
-    public void AdicionarUsuario(Usuario usuario){
-        
-        try{
-            PreparedStatement preparedStatement = connection.prepareStatement("insert into Usuario(tipo,nome,sobrenome,email,senha)");
-        }
-        catch(Exception e){
-            System.out.println("Erro na função Adicionar Usuario!"+ "\n" + e.getMessage());
+
+    public void adicionarUsuario(Usuario usuario) {
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into usuario(tipo,nome,sobrenome,email) values (?, ?, ?, ?) ");
+            preparedStatement.setInt(1, usuario.getTipo());
+            preparedStatement.setString(2, usuario.getNome());
+            preparedStatement.setString(3, usuario.getSobrenome());
+            preparedStatement.setString(4, usuario.getEmail());
+            //  preparedStatement.setString(5, usuario.getSenha());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("Erro na função Adicionar Usuario!" + "\n" + e.getMessage());
         }
     }
+
+    public void deletarUsuario(int id) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("delete from usuario where id=?");         
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("Erro na função Deletar Usuario!" + "\n" + e.getMessage());
+        }
+    }
+    public void atualizarUsuario(Usuario usuario) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("update usuario set nome=?, sobrenome=?, email=?, senha=? where id=?");
+            preparedStatement.setString(1, usuario.getNome());
+            preparedStatement.setString(2, usuario.getSobrenome());
+            preparedStatement.setString(2, usuario.getEmail());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Erro na atualizar usuario!" + "\n" + e.getMessage());
+        }
+    }    
 }
