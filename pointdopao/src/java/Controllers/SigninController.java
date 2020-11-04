940fce3fdef449f7677e1e152db29321f0c244b0
Mathtzt <matheus.cn10@gmail.com>
2020-11-04 09:36:59 -0300

    Criação de arquivo e redirecionamento inicial para tela de login

--- pointdopao/src/java/Controllers/SigninController.java
+++ pointdopao/src/java/Controllers/SigninController.java
@@ -1,49 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controllers;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Matheus
- */
+
 @WebServlet(name = "SigninController", urlPatterns = {"/entrar"})
 public class SigninController extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    private static String ENTRAR = "/entrar.jsp";
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet SigninController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet SigninController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        String forward = ENTRAR;
+        RequestDispatcher view = request.getRequestDispatcher(forward);
+        view.forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
---
+++ pointdopao/web/entrar.jsp
@@ -0,0 +1,11 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Point do Pão</title>
+    </head>
+    <body>
+        <h1>Em construção...</h1>
+    </body>
+</html>
