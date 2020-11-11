3e5a7c3d0500e9609a84a8d60b703c94d4d018a5
bcovies <bcovies@gmail.com>
2020-11-11 11:16:04 -0300

    consertado erros de sintaxe do produtoDao.java

--- pointdopao/src/java/Dao/produtoDao.java
+++ pointdopao/src/java/Dao/produtoDao.java
@@ -5,7 +5,6 @@
  */
 package Dao;
 
-
 import Models.Produto;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -15,28 +14,22 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-
 /**
  *
  * @author Rodrigo
  */
+public class ProdutoDao {
 
+    private String jdbcURL = "jdbc:postgresql://localhost:12002/pointdopao";
 
+    private String jdbcUsername = "admin";
+    private String jdbcPassword = "admin";
 
-public class produtoDao {
-      private String jdbcURL = "jdbc:postgresql://localhost:12002/pointdopao";
-      
-      private String jdbcUsername = "admin";
-      private String jdbcPassword = "admin";
-      
-       private static final String INSERT_PRODUTO_SQL = "INSERT INTO produto (nome, preco, descricao, quantidade) VALUES (?,?,?,?,?);";
-       
-       private static final String SELECT_produto_by_nome = "SELECT nome FROM produto WHERE nome = ?";
+    private static final String INSERT_PRODUTO_SQL = "INSERT INTO produto (nome, preco, descricao, quantidade) VALUES (?,?,?,?,?);";
+    private static final String SELECT_PRODUTO_BY_NOME = "SELECT nome FROM produto WHERE nome = ?";
+    private static final String UPDATE_PRODUTO_QUANTIDADE_BY_NOME = "UPDATE produto SET quantidade = ? WHERE nome = ?";
 
-
-       private static final String UPDATE_produto_PASS_BY_NOME = "UPDATE produto SET quantidade = ? WHERE nome = ?";
-       
-         protected Connection getConnection() {
+    protected Connection getConnection() {
         Connection connection = null;
         System.out.println(INSERT_PRODUTO_SQL);
         try {
@@ -51,8 +44,8 @@ public class produtoDao {
         }
         return connection;
     }
-       
-   public void insertProduto(Produto p) throws SQLException {
+
+    public void insertProduto(Produto p) throws SQLException {
         System.out.println(INSERT_PRODUTO_SQL);
         // try-with-resource statement will auto close the connection.
         try (Connection connection = getConnection();
@@ -61,79 +54,13 @@ public class produtoDao {
             preparedStatement.setFloat(2, p.getPreco());
             preparedStatement.setString(3, p.getDescricao());
             preparedStatement.setInt(4, p.getQuantidade());
-            
-            System.out.println(preparedStatement);
+
+            System.out.println("\nINSERTPRODUTO: " + preparedStatement);
             preparedStatement.executeUpdate();
         } catch (Exception e) {
             e.getMessage();
         }
     }
+
    
-    public Boolean searchUser(String nome) {
-        System.out.println(SELECT_produto_by_nome);
-        boolean autenticado = false;
-
-        try (Connection connection = getConnection();
-                PreparedStatement preparedStatement = connection.prepareStatement(SELECT_produto_by_nome)) {
-
-            preparedStatement.setString(1, nome);
-
-            ResultSet rs;
-            rs = preparedStatement.executeQuery();
-
-            if (rs.next()) {
-                String nomeBanco = rs.getString("nome");
-             
-                autenticado = true;
-                System.out.println("RETORNO BOOL: " + nomeBanco);
-            }
-
-            System.out.println(preparedStatement);
-            preparedStatement.executeUpdate();
-        } catch (Exception e) {
-            e.getMessage();
-        }
-        return autenticado;
-    }
-
-       public boolean changeUserPass(int quantidade, String nome) {
-
-        boolean autenticado = false;
-
-        try (Connection connection = getConnection();
-                PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_produto_PASS_BY_NOME)) {
-            preparedStatement.setString(1, nome);
-            
-
-            ResultSet rs;
-            rs = preparedStatement.executeQuery();
-
-            if (rs.next()) {
-                String nomeBanco = rs.getString("nome");
-                int row = rs.getRow();
-
-                if (row == 1) {
-                    try (PreparedStatement preparePostCheck = connection.prepareStatement(UPDATE_produto_PASS_BY_NOME)) {
-
-                        preparePostCheck.setString(2, nome);
-                        preparePostCheck.setInt(1, quantidade);
-                        System.out.println(preparePostCheck);
-                        preparePostCheck.executeUpdate();
-                        autenticado = true;
-                    }
-
-                } else {
-                    autenticado = false;
-                    System.out.println("NOME NÃO ENCONTRADO!!");
-                }
-
-            }
-
-        } catch (Exception e) {
-
-            e.getMessage();
-        }
-        return autenticado;
-    }
-    
 }
